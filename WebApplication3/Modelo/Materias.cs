92a97a19684bc95005676fc1bed9b8ae4f870de1
adrian <adrian@google.com>
2023-10-16 10:13:16 -0400

    Agregando clase modelo - Materias

---
+++ WebApplication3/Modelo/Materias.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace WebApplication3.Modelo
+{
+    public class Materias
+    {
+        public BigInteger idMateria {  get; set; }
+        public string Nombre { get; set; }
+        public BigInteger idDocente { get; set; }
+    }
+}
